096f7fb46c33e3344292b253c783e8d6e5dd5c9a
Mohammed Aamir Sheriff <aamirsheriff@gmail.com>
2025-08-09 12:56:28 +0530

    Adding Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import Footer from "./components/blocks/Footer";
 import Header from "./components/blocks/Header";
 
 function App() {
-  return <><Header /> </>;
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
--- src/components/blocks/Header.tsx
+++ src/components/blocks/Header.tsx
@@ -3,7 +3,7 @@ import Nav from "../common/Nav";
 
 function Header() {
   return (
-    <header className="fixed w-full z-100 flex justify-between items-center bg-white text-black h-16 shadow-lg px-10">
+    <header className="w-full sticky-top z-100 flex justify-between items-center bg-white text-black h-16 shadow-lg px-10">
       <Logo />
       <Nav />
     </header>
